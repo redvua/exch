813f3c97f4b342cf5fec706e004131f53dc3e117
Ed B <redvepr@gmail.com>
2018-04-23 10:30:17 -0400

    the end

--- exch.sh
+++ exch.sh
@@ -1,4 +1,12 @@
-ex=$(cat privatbank.json | jq '.[]|select(.ccy=="USD").sale')
+if [ $2 != "USD" -a $2 != "EUR" -a $2 != "RUR" ]
+then
+  echo "currency not found"
+  exit 1
+##elif [ !$2 ]
+##then
+fi
+
+ex=$(cat privatbank.json | jq ".[]|select(.ccy==\"$2\").sale")
 ex=${ex:1:-1}
-echo $ex
+echo "scale=2; ($1 * $ex) / 1" | bc
 
